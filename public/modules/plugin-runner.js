2be357a8bc7db069ed076cfd7174bd9e65efa400
唐武兵 <862605874@qq.com>
2026-05-21 10:54:05 +0800

    chore: bump webgl preview version to 2026.05.21.01

--- public/modules/plugin-runner.js
+++ public/modules/plugin-runner.js
@@ -1,5 +1,5 @@
 const PLUGIN_ID = "webgl-preview";
-const WEBGL_PREVIEW_VERSION = "2026.05.20.8";
+const WEBGL_PREVIEW_VERSION = "2026.05.21.01";
 const UNITY_PREVIEW_VERSE_EXPAND =
   "id,name,description,data,metas,metas.code,metas.metaCode,resources,code,uuid,verseCode";
 const SNAPSHOT_EXPAND =
